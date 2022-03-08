a069bee0e6db1222926bacfb4904b2677e628d05
Maurício <mauriciotomazz@gmail.com>
2022-03-07 22:20:01 -0800

    Implement Error (404) page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../assets/wrappers/ErrorPage';
+
 const Error = () => {
-  return <h1>Error</h1>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="This page wasn't found" />
+        <h3>Oops! Page Not Found</h3>
+        <p>We can't seem to find the page you are looking for</p>
+        <Link to="/">Back home</Link>
+      </div>
+    </Wrapper>
+  )
 }
 
 export default Error
