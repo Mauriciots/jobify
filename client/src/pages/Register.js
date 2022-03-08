f57ebf092dabb3a14593c574aeccde9a24c14a37
Maurício <mauriciotomazz@gmail.com>
2022-03-07 22:37:06 -0800

    Implement Register form: first version

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,75 @@
+import { useState, useEffect } from 'react'
+import Wrapper from '../assets/wrappers/RegisterPage'
+import { FormRow, Logo } from '../components';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+}
+
 const Register = () => {
-  return <h1>Register</h1>;
+  const [values, setValues] = useState(initialState);
+
+  const handleInputChange = (e) => {
+    setValues((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  }
+
+  const toggleIsMember = () => {
+    setValues((prevState) => ({ ...prevState, isMember: !prevState.isMember }));
+  }
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={handleFormSubmit}>
+        <Logo />
+        <h3>{values.isMember ? 'Login' : 'Register'}</h3>
+
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={handleInputChange}
+          />
+        )}
+
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          onChange={handleInputChange}
+        />
+
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleInputChange}
+        />
+
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+
+        <p>
+          {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+          <button type="button" onClick={toggleIsMember} className="member-btn">
+            {values.isMember ? 'Register' : 'Login'}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  )
 }
 
 export default Register
